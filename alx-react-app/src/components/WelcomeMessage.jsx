16f787fe533af55cff301f997395746571be6188
zfarmertic <lilyigbunu@gmail.com>
2026-01-08 17:06:22 +0100

    welcome component created

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WelcomeMessage() {
   return (
     <div>
-            <h1>"Hello everyone, I am learning React at ALX"</h1>
+            <h1>Hello everyone, I am learning React at ALX!</h1>
             <p>This is a simple JSX component.</p>
             <p>I am learning about JSX</p>
     </div>
