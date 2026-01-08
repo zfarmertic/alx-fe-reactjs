70271461999f42b9231634c69ffdcb131532c05c
zfarmertic <lilyigbunu@gmail.com>
2026-01-08 17:24:32 +0100

    Added profile components

---
+++ alx-react-app/src/components/UserProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function UserProfile(props) {
+  return (
+    <div>
+        <h2>{props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Bio: {props.bio}</p>
+    </div>
+  )
+}
+
+export default UserProfile
