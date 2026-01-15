2ea6aec74c7f4bb80e67d92d349794fffe63fd57
zfarmertic <lilyigbunu@gmail.com>
2026-01-15 23:03:09 +0100

    new styles added

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+
+function App() {
+  
+
+  return (
+    <>
+      WelcomeMessage, <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer>
+            <p>© 2023 City Lovers</p>
+        </footer>
+    </div>
+  )
+}
+
+export default Footer
