c9807f0a11a9575905c1170fab588bc4f7a26ce2
zfarmertic <lilyigbunu@gmail.com>
2026-01-16 21:24:02 +0100

    commited

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,7 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
-import Counter from '../Counter'
+import Counter from './components/Counter'
 
 function App() {
   
